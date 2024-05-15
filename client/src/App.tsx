1595f47419afe53c8167d08ac49930bae20bbd28
JASEEL T A <jaseelta1@gmail.com>
2024-05-15 23:17:48 +0530

    responsive login page designed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,28 @@
-
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import EditProfile from "./pages/EditProfile";
+import NotFound from "./pages/NotFound";
+import Register from "./pages/Register";
+import Error from "./pages/Error";
 
 function App() {
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/edit-profile" element={<EditProfile />} />
+          <Route path="/error" element={<Error />} /> 
+          <Route path="/*" element={<NotFound />} /> 
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/EditProfile.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function EditProfile() {
+  return (
+      <>
+         <h1>Edit-Profile Page</h1> 
+          
+      </>
+  )
+}
+
+export default EditProfile
